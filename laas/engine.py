17736fd8996a0db8b71f0ef8403c51380f11ae1d
Jacek Płocharczyk <jacek.plocharczyk@mowa.ai>
2023-08-16 08:39:03 +0000

    move processing to engine.py

---
+++ laas/engine.py
@@ -0,0 +1,31 @@
+from llama import Llama
+import config
+import api_models
+
+MODEL = None
+
+
+def init_model():
+    global MODEL
+    if not MODEL:
+        print("loading model")
+        MODEL = Llama.build(
+            ckpt_dir=config.CHECKPOINT_DIR,
+            tokenizer_path=config.TOKENIZER_PATH,
+            max_seq_len=config.MAX_SEQ_LEN,
+            max_batch_size=config.MAX_BATCH_SIZE,
+            model_parallel_size=config.MODEL_PARALLEL_SIZE,
+        )
+        print("model loaded")
+
+
+def process_message(prompt: str):
+    dialog = [{"role": "user", "content": prompt}]
+
+    results = MODEL.chat_completion(
+        [dialog],
+        max_gen_len=config.MAX_GEN_LEN,
+        temperature=config.TEMPERATURE,
+        top_p=config.TOP_P,
+    )
+    return results[0]
--- laas/main.py
+++ laas/main.py
@@ -3,41 +3,21 @@
 
 from fastapi import FastAPI
 
-
-from llama import Llama
 import config
+import engine
 
-MODEL = None
 
 app = FastAPI()
 
 
 @app.on_event("startup")
-async def startup_event():
-    global MODEL
-    if not MODEL:
-        print("loading model")
-        MODEL = Llama.build(
-            ckpt_dir=config.CHECKPOINT_DIR,
-            tokenizer_path=config.TOKENIZER_PATH,
-            max_seq_len=config.MAX_SEQ_LEN,
-            max_batch_size=config.MAX_BATCH_SIZE,
-            model_parallel_size=config.MODEL_PARALLEL_SIZE,
-        )
-        print("model loaded")
+async def startup():
+    engine.init_model()
 
 
 @app.post("/process_message")
-async def engine(prompt: str):
-    dialog = [{"role": "user", "content": prompt}]
-
-    results = MODEL.chat_completion(
-        [dialog],
-        max_gen_len=config.MAX_GEN_LEN,
-        temperature=config.TEMPERATURE,
-        top_p=config.TOP_P,
-    )
-    return results[0]
+async def process_message(prompt: str):
+    return engine.process_message(prompt)
 
 
 if __name__ == "__main__":
